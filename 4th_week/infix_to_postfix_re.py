b662bfe39a32c452035047cb35dbe4fa292dbf69
jbw9964 <jbw9964@gmail.com>
2022-07-30 15:25:18 +0900

    [Solution] : 4th_week problem_1 solution has been updated

--- 4th_week/infix_to_postfix_re.py
+++ 4th_week/infix_to_postfix_re.py
@@ -3,6 +3,7 @@ from collections import deque
 
 def infix_to_postfix_fun() -> list : 
     
+    # 1. 입력받은 문자열을 Queue 에 넣은 후, 하나씩 pop 시키며 생각한다. pop 시킨 문자를 text 라 하자.
     infix_string = deque(map(str, input().split()))
 
     Postfix_string = []
@@ -11,8 +12,12 @@ def infix_to_postfix_fun() -> list :
 
     while infix_string : 
         text = infix_string.popleft()
-        
-        if text in '+-*/' :                     # text = +,-,*,/
+    
+        #---------------------------------------------------------------#
+        # 3. text 가 연산자일 경우 Stack 에 넣어야 한다.
+        # 4. text 가 연산자 중 +,-,*,/ 이면, Stack 내부의 연산자 중 자신보다 높거나 같은 우선순위를 가진 연산자들은 모두 pop 시켜 Postfix_string 에 넣어야 한다.
+
+        if text in '+-*/' :                     # text = operator except ()
             
             if text in '+-' :                   # text = +,-
                 
@@ -45,6 +50,9 @@ def infix_to_postfix_fun() -> list :
                     
                 Stack.append(text)
         
+        #---------------------------------------------------------------#
+        # 5. text 가 ( 이면 그냥 Stack 에 넣고, ) 이면 pop 된 연산자가 ( 일 때까지 Stack 을 pop 시켜 Postfix_string 에 넣어야 한다.
+
         elif text == '(' :                      # text = (
             Stack.append(text)
         elif text == ')' :                      # text = )
@@ -57,8 +65,16 @@ def infix_to_postfix_fun() -> list :
                 else : 
                     Postfix_string.append(in_stack)
 
+        #---------------------------------------------------------------#
+        # 2. text 가 숫자일 경우 Postfix_string 에 넣는다.
+
         else :                                  # text = number
             Postfix_string.append(int(text))
+
+        #---------------------------------------------------------------#
+        # end while
+
+    # 6. 모든 text 를 둘러보았을 때, Stack 내부에 남아있는 연산자들을 모두 Postfix_string 에 넣는다.
 
     while Stack : 
         in_stack = Stack.pop()
--- 4th_week/problem_1.py
+++ 4th_week/problem_1.py
@@ -14,7 +14,10 @@ def postfix_calc_fun(Postfix : list) -> int :
 
     for text in Postfix : 
 
-        if type(text) == str :          # num_1 num_2 (text) --> num_1 (text) num_2
+        #------------------------------------------------------------#
+        # 3. text 가 연산자일 경우, Stack 에서 2 개를 pop 해 서로 연산을 진행하고 결과를 다시 Stack 에 넣는다.
+
+        if type(text) == str :                              # num_1 num_2 (text) --> num_1 (text) num_2
             num_2 = Stack.pop()
             num_1 = Stack.pop()
             
@@ -23,8 +26,14 @@ def postfix_calc_fun(Postfix : list) -> int :
             elif text == '*' : Stack.append(num_1 * num_2)
             else : Stack.append(num_1 / num_2)              # a b / --> a / b
         
+        #------------------------------------------------------------#
+        # 2. text 가 숫자일 경우 Stack 에 넣는다.
+
         else : 
             Stack.append(int(text))
+        
+        #------------------------------------------------------------#
+        # end for
         
     return Stack[0]
 
